aaf0094376af889df59f64c1ab9655f4e4e3e081
Nebil Abdulfetah <nebilabdu300@gmail.com>
2022-04-16 10:16:00 -0400

    collabrative work on tasks

---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+void print_string(char *s, int char_count);
+void print_number(int n, int char_count);
+
+#endif
