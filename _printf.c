0d7a33636fe23332e3abed2da894d286abe90140
EyobABN <eyob.alamerew.belay@gmail.com>
2022-04-17 02:31:20 +0300

    Refactor _printf.c

--- _printf.c
+++ _printf.c
@@ -3,6 +3,21 @@
 #include <stddef.h>
 
 /**
+ * process_putchar - processes a _putchar call
+ * @c: the character to be written
+ * @char_count: the character count so far
+ * @i: the index
+ *
+ * Return: void
+ */
+void process_putchar(char c, int *char_count, int *i)
+{
+	_putchar(c);
+	(*char_count)++;
+	*i += 2;
+}
+
+/**
  * _printf - a printf function
  * @format: the format
  *
@@ -12,6 +27,7 @@ int _printf(const char *format, ...)
 {
 	va_list ap;
 	int i, char_count;
+	char *s;
 
 	va_start(ap, format);
 	char_count = 0;
@@ -19,20 +35,15 @@ int _printf(const char *format, ...)
 	while (format[i] != '\0')
 	{
 		if (format[i] == '%' && format[i + 1] == 'c')
-		{
-			_putchar(va_arg(ap, int));
-			char_count++;
-			i += 2;
-		}
+			process_putchar(va_arg(ap, int), &char_count, &i);
 		else if (format[i] == '%' && format[i + 1] == '%')
-		{
-			_putchar('%');
-			char_count++;
-			i += 2;
-		}
+			process_putchar('%', &char_count, &i);
 		else if (format[i] == '%' && format[i + 1] == 's')
 		{
-			print_string(va_arg(ap, char *), &char_count);
+			s = va_arg(ap, char *);
+			if (s == NULL)
+				s = "(null)";
+			print_string(s, &char_count);
 			i += 2;
 		}
 		else if (format[i] == '%' && (format[i + 1] == 'd' || format[i + 1] == 'i'))
